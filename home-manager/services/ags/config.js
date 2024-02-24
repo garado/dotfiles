01aab0c38769dc30e829cdafcb5126110b476405
garado <alexisgarado@gmail.com>
2024-02-24 23:16:14 +0000

    [ags] add feather icons and nix icon

--- home-manager/services/ags/config.js
+++ home-manager/services/ags/config.js
@@ -1,5 +1,10 @@
 import App from 'resource:///com/github/Aylur/ags/app.js'
 import Utils from 'resource:///com/github/Aylur/ags/utils.js'
+import Gtk from 'gi://Gtk?version=3.0';
+
+// Custom icons
+Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets/icons`);
+Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets/icons/feather`);
 
 // Compile SASS
 const sass = `${App.configDir}/sass/base.sass`
