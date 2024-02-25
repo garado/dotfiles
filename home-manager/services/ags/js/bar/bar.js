14943b9b2ac615f5bd8e087da0d9b670437ef8d8
garado <alexisgarado@gmail.com>
2024-02-25 08:39:15 +0000

    [ags] add battery percent to bar

--- home-manager/services/ags/js/bar/bar.js
+++ home-manager/services/ags/js/bar/bar.js
@@ -1,6 +1,8 @@
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js'
 import Widget from 'resource:///com/github/Aylur/ags/widget.js'
 import Variable from 'resource:///com/github/Aylur/ags/variable.js'
+const battery = await Service.import('battery')
+
 
 function Workspaces() {
   const workspaces = Hyprland.bind('workspaces');
@@ -23,6 +25,14 @@ const Datetime = Widget.Label({
   label: date.bind()
 })
 
+const batteryPercent = battery.bind('percent').as(p => `${p}`)
+const batteryCharging = battery.bind('charging')
+
+const Battery = Widget.Label({
+  label: batteryPercent,
+  class_name: batteryCharging.value ? 'greentext' : ''
+})
+
 // ------------
 
 const Left = Widget.Box({
@@ -40,7 +50,12 @@ const Center = Widget.Box({
 const Right = Widget.Box({
   hpack: 'end',
   class_name: 'right',
-  children: [Datetime]
+  vertical: false,
+  spacing: 7,
+  children: [
+    Battery,
+    Datetime,
+  ]
 })
 
 export default (monitor = 0) => Widget.Window({
