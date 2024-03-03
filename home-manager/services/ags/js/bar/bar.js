d2dac525f789b12e902abfcd41065757e12d36a6
garado <alexisgarado@gmail.com>
2024-03-03 19:30:19 +0000

    [ags] bar: switch to vertical

--- home-manager/services/ags/js/bar/bar.js
+++ home-manager/services/ags/js/bar/bar.js
@@ -1,3 +1,7 @@
+
+// █▄▄ ▄▀█ █▀█
+// █▄█ █▀█ █▀▄
+
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js'
 import Widget from 'resource:///com/github/Aylur/ags/widget.js'
 import Variable from 'resource:///com/github/Aylur/ags/variable.js'
@@ -8,65 +12,90 @@ function Workspaces() {
   const workspaces = Hyprland.bind('workspaces');
   const activeId = Hyprland.active.workspace.bind('id');
   return Widget.Box({
+    vertical: true,
+    spacing: 2,
+    hpack: 'center',
     class_name: 'workspaces',
-    children: workspaces.as(ws => ws.map(({ id }) => Widget.EventBox ({
+    children: workspaces.as(ws => ws.map(({ id }) => Widget.EventBox({
       on_clicked: () => Hyprland.messageAsync(`dispatch workspace ${id}`),
-      child: Widget.Label(`${id}`),
+      child: Widget.Label({
+        hpack: 'center',
+        label: `${id}`
+      }),
       class_name: activeId.as(i => `${i === id ? 'focused' : ''}`),
     }))),
   });
 }
 
-const date = Variable('', {
-  poll: [1000, "date '+%H:%M'"],
-})
+const Time = () => {
+  const hour = Variable('', {
+    poll: [1000, "date '+%H'"],
+  })
+  
+  const min = Variable('', {
+    poll: [1000, "date '+%M'"],
+  })
 
-const Datetime = Widget.Label({
-  label: date.bind()
-})
+  return Widget.Box({
+    vertical: true,
+    children: [
+      Widget.Label({ label: hour.bind() }),
+      Widget.Label({ label: min.bind() }),
+    ]
+  })
+}
 
-const batteryPercent = battery.bind('percent').as(p => `${p}`)
-const batteryCharging = battery.bind('charging')
+const Battery = () => {
+  const batteryPercent = battery.bind('percent').as(p => `${p}`)
+  const batteryCharging = battery.bind('charging')
 
-const Battery = Widget.Label({
-  label: batteryPercent,
-  class_name: batteryCharging.value ? 'greentext' : ''
-})
+  return Widget.Label({
+    label: batteryPercent,
+    class_name: batteryCharging.value ? 'greentext' : ''
+  })
+}
 
 // ------------
 
-const Left = Widget.Box({
-  class_name: 'left',
+const Top = Widget.Box({
+  vpack: 'start',
+  hpack: 'center',
+  className: 'top',
   children: [
     Widget.Icon({icon: 'nix'})
   ]
 })
 
 const Center = Widget.Box({
+  vpack: 'center',
+  hpack: 'center',
   class_name: 'center',
   children: [Workspaces()]
 })
 
-const Right = Widget.Box({
-  hpack: 'end',
+const Bottom = Widget.Box({
+  vpack: 'end',
+  hpack: 'center',
   class_name: 'right',
-  vertical: false,
-  spacing: 7,
+  vertical: true,
   children: [
-    Battery,
-    Datetime,
+    Battery(),
+    Time(),
   ]
 })
 
 export default (monitor = 0) => Widget.Window({
   name: `bar-${monitor}`, // name has to be unique
   monitor,
-  anchor: ['top', 'left', 'right'],
+  anchor: ['left', 'top', 'bottom'],
+  vexpand: true,
   exclusivity: 'exclusive',
   child: Widget.CenterBox({
+    vertical: true,
+    hpack: 'center',
     class_name: 'bar',
-    start_widget: Left,
+    start_widget: Top,
     center_widget: Center,
-    end_widget: Right,
+    end_widget: Bottom,
   }),
 });
