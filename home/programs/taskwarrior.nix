2e13eef2023d74e65cf249499f5e62db2f5e2b3a
garado <alexisgarado@gmail.com>
2025-01-07 20:49:35 -0800

    [home] define taskwarrior task contexts

--- home/programs/taskwarrior.nix
+++ home/programs/taskwarrior.nix
@@ -2,20 +2,23 @@
 # ▀█▀ ▄▀█ █▀ █▄▀ █░█░█ ▄▀█ █▀█ █▀█ █ █▀█ █▀█
 # ░█░ █▀█ ▄█ █░█ ▀▄▀▄▀ █▀█ █▀▄ █▀▄ █ █▄█ █▀▄
 
-{...}: {
+{ pkgs, ... }: {
   programs.taskwarrior = {
     enable = true;
+    
+    package = pkgs.taskwarrior3;
 
-    dataLocation = "/home/alexis/Enchiridion/todo/.taskwiki/";
+    dataLocation = "/home/alexis/Enchiridion/self/todo/.taskwiki/";
 
     # User-defined attributes
     config = {
       # UDAs for goals
-      "uda.imgpath.type" = "string";
-      "uda.imgpath.label" = "imagepath";
-
       "uda.why.type" = "string";
       "uda.why.label" = "why";
+
+      # Set context to display goals
+      "context.goals" = "tag:goals";
+      "context.todo" = "tag:books or tag:personal or tag:misc or tag:desktop";
     };
   };
 }
